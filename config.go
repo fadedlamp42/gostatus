4e7087ac322aa0b33beabf4d113bd5ff0b0e21f4
Peter <pedro@charmetric.com>
2021-09-06 12:20:48 -0400

    update hp config

--- config.go
+++ config.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"github.com/fadedlamp42/gostatus/model"
 	"github.com/fadedlamp42/gostatus/components"
+	"github.com/fadedlamp42/gostatus/model"
 )
 
-const UPDATES_PER_SECOND = 0.5
+const UPDATES_PER_SECOND = 1
 
 func buildTopBar() model.Bar {
 	return model.Bar{
 		Seperator: '|',
-		Edges: [2]string {"[", "]"},
+		Edges:     [2]string{"[", "]"},
 		Components: []model.Component{
 			&components.Time{},
 			components.NewCPU(),
@@ -24,10 +24,11 @@ func buildTopBar() model.Bar {
 func buildBottomBar() model.Bar {
 	return model.Bar{
 		Seperator: '|',
-		Edges: [2]string{"", ""},
+		Edges:     [2]string{"", ""},
 		Components: []model.Component{
 			components.NewBattery("BAT0"),
-			components.NewBatteryBar("BAT0", 50),
+			components.NewBatteryBar("BAT0", 20, false),
+			&components.Command{Text: "hp-battery.sh"},
 		},
 	}
 }
